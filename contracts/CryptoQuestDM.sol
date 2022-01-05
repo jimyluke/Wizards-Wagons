aa999545c7894e1db6cb95b8b376ef4ed181462c
jimyluke <myzsoftdev1@gmail.com>
2022-01-05 09:08:25 -0500

    add presale

--- contracts/CryptoQuestDM.sol
+++ contracts/CryptoQuestDM.sol
@@ -27,6 +27,15 @@ contract CryptoQuestDM is ERC721Enumerable, Ownable, ERC721Burnable, ERC721Pausa
     string public baseTokenURI;
     uint256 public basePrice = 0.07 ether;
     uint256 public batch = 0;
+    uint256 public preSaleCost = 0.05 ether;
+    uint256 public preSaleMaxSupply = 2250;
+    uint256 public maxMintAmountPresale = 1;
+    uint256 public maxMintAmount = 10;
+    uint256 public nftPerAddressLimitPresale = 1;
+    uint256 public nftPerAddressLimit = 100;
+    uint256 public preSaleDate = 1638738000;
+    uint256 public preSaleEndDate = 1638824400;
+    uint256 public publicSaleDate = 1638842400;
 
     event CreateNft(uint256 indexed id);
     event AttributeChanged(uint256 indexed _tokenId, string _key, string _value);
@@ -35,6 +44,9 @@ contract CryptoQuestDM is ERC721Enumerable, Ownable, ERC721Burnable, ERC721Pausa
         setBaseURI(baseURI);
         sale = false;
     }
+
+    mapping(address => bool) whitelistedAddresses;
+    mapping(address => uint256) public addressMintedBalance;
 
     modifier saleIsOpen {
         require(_totalSupply() <= MAX_ITEMS*batch, "Sale ended");
@@ -45,27 +57,20 @@ contract CryptoQuestDM is ERC721Enumerable, Ownable, ERC721Burnable, ERC721Pausa
         _;
     }
 
-    mapping(address => bool) whitelistedAddresses;
+    // modifier onlyOwner {
+    //     require(msg.sender == owner, "Ownable: caller is not the owner");
+    //     _;
+    // }
 
-    modifier onlyOwner2() {
-      require(msg.sender == devAddress, "Ownable: caller is not the owner");
-      _;
+    modifier saleStarted {
+        require(block.timestamp >= preSaleDate, "Sale has not started yet");
+        _;
     }
 
-    modifier isWhitelisted(address _address) {
-      require(whitelistedAddresses[_address], "Whitelist: You need to be whitelisted");
-      _;
-    }
-
-    function addUser(address _addressToWhitelist) public onlyOwner {
-      whitelistedAddresses[_addressToWhitelist] = true;
-    }
-
-    function verifyUser(address _whitelistedAddress) public view returns(bool) {
-      bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
-      return userIsWhitelisted;
-    }
-
+    // modifier isWhitelisted(address _address) {
+    //     require(whitelistedAddresses[_address], "You need to be whitelisted");
+    //     _;
+    // }
 
     function _totalSupply() internal view returns (uint) {
         return _tokenIdTracker.current();
@@ -75,20 +80,59 @@ contract CryptoQuestDM is ERC721Enumerable, Ownable, ERC721Burnable, ERC721Pausa
         return _totalSupply();
     }
 
-    function allowToMint() public view isWhitelisted(msg.sender) returns(bool){
-      return (true);
-    }
     function mint(address _to, uint256 _count) public payable saleIsOpen {
         uint256 total = _totalSupply();
+        uint256 ownerMintedCount = addressMintedBalance[msg.sender];
         require(sale == false, "Sale has not yet started");
         require(total <= MAX_ITEMS*batch, "Sale ended");
         require(total + _count <= MAX_ITEMS*batch, "Max limit");
         require(_count <= MAX_MINT, "Exceeds number");
         require(msg.value >= price(_count), "Value below price");
 
+        block.timestamp < publicSaleDate
+        ? presaleValidations(ownerMintedCount, _count, total)
+        : publicsaleValidations(ownerMintedCount, _count);
+
         for (uint256 i = 0; i < _count; i++) {
             _mintAnElement(_to);
         }
+    }
+
+    // function mint(uint256 _mintAmount)
+    //     public
+    //     payable
+    //     notPaused
+    //     saleStarted
+    //     minimumMintAmount(_mintAmount)
+    // {
+    //     uint256 supply = totalSupply();
+    //     uint256 ownerMintedCount = addressMintedBalance[msg.sender];
+
+    //     //Do some validations depending on which step of the sale we are in
+    //     block.timestamp < publicSaleDate
+    //     ? presaleValidations(ownerMintedCount, _mintAmount, supply)
+    //     : publicsaleValidations(ownerMintedCount, _mintAmount);
+
+    //     require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
+
+    //     for (uint256 i = 1; i <= _mintAmount; i++) {
+    //     addressMintedBalance[msg.sender]++;
+    //     _safeMint(msg.sender, supply + i);
+    //     }
+    // }
+
+    function publicsaleValidations(uint256 _ownerMintedCount, uint256 _mintAmount)
+        internal
+    {
+        require(
+        _ownerMintedCount + _mintAmount <= nftPerAddressLimit,
+        "max NFT per address exceeded"
+        );
+        require(msg.value >= basePrice * _mintAmount, "insufficient funds");
+        require(
+        _mintAmount <= maxMintAmount,
+        "max mint amount per transaction exceeded"
+        );
     }
 
     function _mintAnElement(address _to) private {
@@ -172,5 +216,51 @@ contract CryptoQuestDM is ERC721Enumerable, Ownable, ERC721Burnable, ERC721Pausa
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+
+    function addUser (address _addressToWhitelist) public onlyOwner {
+        whitelistedAddresses[_addressToWhitelist] = true;
+    }
+
+    function verifyUser(address _whitelistedAddress) public view returns(bool) {
+        bool userIsWhitelisted = whitelistedAddresses[_whitelistedAddress];
+        return userIsWhitelisted;
+    }
+
+    function presaleValidations(
+        uint256 _ownerMintedCount,
+        uint256 _mintAmount,
+        uint256 _supply
+    ) internal {
+        uint256 actualCost;
+        block.timestamp < preSaleEndDate
+        ? actualCost = preSaleCost
+        : actualCost = basePrice;
+        require(isWhitelisted(msg.sender), "user is not whitelisted");
+        require(
+        _ownerMintedCount + _mintAmount <= nftPerAddressLimitPresale,
+        "max NFT per address exceeded for presale"
+        );
+        require(msg.value >= actualCost * _mintAmount, "insufficient funds");
+        require(
+        _mintAmount <= maxMintAmountPresale,
+        "max mint amount per transaction exceeded"
+        );
+        require(
+        _supply + _mintAmount <= preSaleMaxSupply,
+        "max NFT presale limit exceeded"
+        );
+    }
+
+    function setNftPerAddressLimitPreSale(uint256 _limit) public onlyOwner {
+        nftPerAddressLimitPresale = _limit;
+    }
+
+    function setPresaleCost(uint256 _newCost) public onlyOwner {
+        preSaleCost = _newCost;
+    }
+
+    function isWhitelisted(address _user) public view returns (bool) {
+        return whitelistedAddresses[_user];
     }
 }
